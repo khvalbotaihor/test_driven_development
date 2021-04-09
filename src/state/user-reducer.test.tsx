268a37d853d916d284a973c40328da79d76d4240
khvalbotaihor <ihorkhvalbota@gmail.com>
2021-04-09 05:54:18 +0300

    - add action creators to tests

--- src/state/user-reducer.test.tsx
+++ src/state/user-reducer.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {ChangeNameAC, userReducer} from './user-reducer';
+import {ChangeNameAC, IncrementChildrenCountAC, IncrementUserAC, userReducer} from './user-reducer';
 
 test('user reducer should increment only age', () => {
     const startState = { age: 20, childrenCount: 2, name: 'Dimych' };
-
-    const endState = userReducer(startState, { type: 'INCREMENT-AGE' })
+    const action = IncrementUserAC();
+    const endState = userReducer(startState, action);
 
     expect(endState.age).toBe(21);
     expect(endState.childrenCount).toBe(2);
@@ -12,7 +12,8 @@ test('user reducer should increment only age', () => {
 
 test('user reducer should increment only childrenCount', () => {
     const startState = { age: 20, childrenCount: 2, name: 'Dimych' };
-    const endState = userReducer(startState, { type: 'INCREMENT-CHILDREN-COUNT' })
+    const action = IncrementChildrenCountAC();
+    const endState = userReducer(startState, action);
 
     expect(endState.age).toBe(20);
     expect(endState.childrenCount).toBe(3);
@@ -21,9 +22,7 @@ test('user reducer should increment only childrenCount', () => {
 test('user reducer should change name of user', () => {
     const startState = { name: 'Dimych', age: 20, childrenCount: 2 };
     const newName = 'Viktor';
-
     const action = ChangeNameAC(newName)
-
     const endState = userReducer(startState, action)
 
     expect(endState.name).toBe(newName);
